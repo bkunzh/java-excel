4567bba8d4b10c93724db4ce354945e4052e2c13
bkunzh <dreamkunk@foxmail.com>
2020-06-28 11:16:01 +0800

    joda-time test

---
+++ src/test/java/JodaTimeTest.java
@@ -0,0 +1,25 @@
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+
+import java.util.Calendar;
+
+public class JodaTimeTest {
+    @Test
+    public void t1() {
+        LocalDate now = new LocalDate();
+        LocalDate lastDayOfPreviousMonth = now.minusMonths(1).dayOfMonth().withMaximumValue();
+        System.out.println(lastDayOfPreviousMonth.toString());
+    }
+
+    @Test
+    public void t2() {
+        // Use a Calendar
+        java.util.Calendar calendar = Calendar.getInstance();
+        DateTime dateTime = new DateTime(calendar);
+        // Use another Joda DateTime
+        String timeString = "2006-01-26";
+        dateTime = new DateTime(timeString);
+        System.out.println("dateTime = " + dateTime);
+    }
+}
